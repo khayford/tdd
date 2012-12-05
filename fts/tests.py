7f62945a044cb32ff4eb5fbd187870538b23fa39
kofi.hayford@pallionghana.com <kofi.hayford@gmail.com>
2012-12-05 21:42:43 +0000

    fixed fixtures addition to fts

--- fts/tests.py
+++ fts/tests.py
@@ -1,25 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        
-        Tests that 1 + 1 always equals 2.
-       
-        self.assertEqual(1 + 1, 2)
-"""
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class PollsTest(LiveServerTestCase):
+
+    fixtures = ['admin_user.json']
 
     def setUp(self):
         self.browser = webdriver.Firefox()
